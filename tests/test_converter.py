102e65d9bb6496547ff319f4193e5b43dabf06c4
lpozo <lpozo@openmailbox.org>
2017-01-15 17:58:04 -0500

    Update tests

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -28,16 +28,20 @@ from videomorph.converter import XMLProfile
 
 conv = None
 
+XMLProfile.create_profiles_xml_file()
+XMLProfile.set_xml_root()
 
 # Set of test for Converter class
 def setup():
     media_list = media.MediaList()
+
     media_file = media.MediaFile(
         file_path='Dad.mpg',
         conversion_profile=XMLProfile.get_conversion_profile(
             profile_name='DVD',
             target_quality='DVD Fullscreen (4:3)'),
         prober='ffprobe')
+
     media_list.add_file(media_file)
     global conv
     conv = converter.Converter(media_list)
