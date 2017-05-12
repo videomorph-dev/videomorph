828df28570a34c7b6b633f862b0dc30146f40c0f
lpozo <lpozo@openmailbox.org>
2017-05-11 23:54:39 -0400

    Use setuptools.setup instead of disutils.core.setup for more options, but still have the distutils variant for completeness

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 from videomorph.videomorph import VERSION
 
