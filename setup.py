0a74cf1bb93837c76dc5fd08a21a911fa72c132f
lpozo <lpozo@openmailbox.org>
2017-05-14 11:03:10 -0400

    Include a lon description in setup.setup()

--- setup.py
+++ setup.py
@@ -3,11 +3,24 @@ from setuptools import setup, find_packages
 
 from videomorph.videomorph import VERSION
 
+LONG_DESC = """Small Video Converter based on ffmpeg, Python 3 and Qt5.
+Unlike other video converters, VideoMorph focuses on a single task,
+convert video, making it simple, easy to use and allowing the user
+choose from a list of popular video formats.
+
+VideoMorph UI is simple and clean focused on usability, eliminating
+annoying options rarely used.
+Videomorph is a video converter, just that. If you want a video
+editor, VideoMorph isn't for you.
+"""
+
 if __name__ == '__main__':
     setup(name='videomorph',
           version=VERSION,
           description='Small Video Converter based on ffmpeg, '
                       'Python 3 and Qt5, focused on usability.',
+          long_description=LONG_DESC,
+
           author='Ozkar L. Garcell',
           author_email='codeshard@openmailbox.org',
           maintainer='Leodanis Pozo Ramos',
@@ -31,7 +44,7 @@ if __name__ == '__main__':
                         # Documentation files
                       ('/usr/share/doc/videomorph',
                        ['README.md', 'LICENSE', 'AUTHORS',
-                        'copyright', 'changelog.Debian', 'TODO'])],
+                        'copyright', 'changelog.gz', 'TODO'])],
 
           scripts=['bin/videomorph']
           )
