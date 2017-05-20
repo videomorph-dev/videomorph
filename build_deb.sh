2b9e43628220686d3034ceeb9fc8827f1373bfae
lpozo <lpozo@openmailbox.org>
2017-05-20 13:56:09 -0400

    Improve build_deb.sh script to meet all Debian packages requirements. Now it build a consistent deb package with no lintian error

--- build_deb.sh
+++ build_deb.sh
@@ -2,8 +2,9 @@
 
 version="1.0"
 ubuntu="xenial"
+python_version=$(python3 --version | cut -f2 -d " " | cut --fields=1,2 -d ".")
 
-gzip -n -k --best changelog changelog.gz
+gzip -n --keep --force --best changelog
 
 if [ -d dist ]
 then
@@ -18,18 +19,31 @@ python3 setup.py bdist
 # Untar it to videomorph_deb
 tar -xvf dist/videomorph*.tar.gz --directory videomorph_deb
 
+# Addapt to .deb standard structure
+mv -v videomorph_deb/usr/local/* videomorph_deb/usr
+rm -rfv videomorph_deb/usr/local
+rm -rfv videomorph_deb/usr/share/doc/videomorph/LICENSE
+rm -rfv videomorph_deb/usr/share/doc/videomorph/INSTALL
+mv videomorph_deb/usr/lib/python"$python_version"/ videomorph_deb/usr/lib/python3
+
 # Clean
 rm -rfv dist/*
 
 # Remove __pycache__ directories so they are not put into .deb package
 rm -rfv $(find videomorph_deb/ -path "*__pycache__*")
 
+# Change Owner and Group
+sudo chown -c --recursive root:root videomorph_deb/usr/
+sudo chown -c --recursive root:root videomorph_deb/DEBIAN/postinst
+sudo --remove-timestamp
+
 # Build the DEB package
 dpkg-deb --build videomorph_deb/ "videomorph_""$version""_""$ubuntu""_all.deb"
 mv -v "videomorph_""$version""_""$ubuntu""_all.deb" dist
 
 # Some clean up
-rm -rfv build videomorph_deb/usr
+sudo rm -rfv build videomorph_deb/usr
+sudo --remove-timestamp
 rm -rfv changelog.gz
 
 # Build a standard python dist to be install with python3 setup.py install command
