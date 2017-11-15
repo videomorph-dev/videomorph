9e666e462686130b208cfdcaa3d464d2df4161d3
lpozo <lpozo78@nauta.cu>
2017-11-14 21:52:32 -0500

    Update the install.sh script

--- install.sh
+++ install.sh
@@ -3,12 +3,15 @@
 
 depends="ffmpeg python3 python3-pyqt5"
 
-for dep in $depends
-do
-    if ! dpkg-query -l "$dep" | grep "ii" &> /dev/null
-    then
-        apt-get install "$dep"
-    fi
-done
+if type dpkg &> /dev/null
+then
+    for dep in $depends
+    do
+        if ! dpkg-query -l "$dep" | grep "ii" &> /dev/null
+        then
+            apt-get install "$dep"
+        fi
+    done
+fi
 
 python3 setup.py install
