541fa2ef33dbde05f9c5f33650807b59df56e144
lpozo <lpozo78@nauta.cu>
2018-01-31 17:25:29 -0500

    Remove avconv from modules general comments

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,7 @@
 
 # File name: test_utils.py
 #
-#   VideoMorph - A PyQt5 frontend to ffmpeg and avconv.
+#   VideoMorph - A PyQt5 frontend to ffmpeg.
 #   Copyright 2016-2017 VideoMorph Development Team
 
 #   Licensed under the Apache License, Version 2.0 (the "License");
