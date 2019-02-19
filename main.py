620351abd1c98cbc255db8ad4de873d91c093952
Leodanis Pozo Ramos <lpozor78@gmail.com>
2019-02-18 22:15:34 -0500

    Modify main.py (client code) and mainctrl.py

--- main.py
+++ main.py
@@ -19,68 +19,10 @@
 
 """This module contains the main function for VideoMorph."""
 
-import sys
-from pathlib import Path
-
-from PyQt5.QtCore import QLibraryInfo
-from PyQt5.QtCore import QTranslator
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QMessageBox
-from PyQt5.QtWidgets import qApp
-
-from videomorph import BASE_DIR
-from videomorph import LOCALE
-from videomorph import SYS_PATHS
-from videomorph import VM_PATHS
-from videomorph.console import run_on_console
-from qtviews.videomorph import VideoMorphMW
+from qtctrl.mainctrl import QtMainController
 
 
 def main():
     """Main app function."""
-    # Create the app
-    app = QApplication(sys.argv)
-
-    # Setup app translator
-    app_translator = QTranslator()
-
-    translator_pwd = Path(BASE_DIR, VM_PATHS.i18n)
-
-    if translator_pwd.exists():
-        app_translator.load(
-            str(translator_pwd.joinpath('videomorph_{0}'.format(LOCALE))))
-    else:
-        translator_sys_path = Path(SYS_PATHS.i18n,
-                                   'videomorph_{0}'.format(LOCALE))
-        app_translator.load(str(translator_sys_path))
-
-    app.installTranslator(app_translator)
-    qt_translator = QTranslator()
-    qt_translator.load("qt_" + LOCALE,
-                       QLibraryInfo.location(QLibraryInfo.TranslationsPath))
-    app.installTranslator(qt_translator)
-
-    # Run the app
-    run_app(app=app)
-
-
-def run_app(app):
-    """Run the app."""
-    # Create the Main Window
-    main_win = VideoMorphMW()
-    # Check for conversion library and run
-    if main_win.conversion_lib.library_path:
-        if len(sys.argv) > 1:  # If it is running from console
-            run_on_console(app, main_win)
-        else:  # Or is running on GUI
-            main_win.show()
-            sys.exit(app.exec_())
-    else:
-        msg_box = QMessageBox(
-            QMessageBox.Critical,
-            main_win.tr('Error!'),
-            main_win.no_library_msg,
-            QMessageBox.NoButton, main_win)
-        msg_box.addButton("&Ok", QMessageBox.AcceptRole)
-        if msg_box.exec_() == QMessageBox.AcceptRole:
-            qApp.closeAllWindows()
+    qt_ctrl = QtMainController()
+    qt_ctrl.run()
--- qtctrl/mainctrl.py
+++ qtctrl/mainctrl.py
@@ -22,9 +22,19 @@
 """This module provides Main Controller for Qt GUI."""
 
 import sys
+from pathlib import Path
 
+from PyQt5.QtCore import QLibraryInfo
+from PyQt5.QtCore import QTranslator
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import qApp
 
+from videomorph import BASE_DIR
+from videomorph import LOCALE
+from videomorph import SYS_PATHS
+from videomorph import VM_PATHS
+from videomorph.console import run_on_console
 from qtviews.videomorph import VideoMorphMW
 
 
@@ -32,10 +42,48 @@ class QtMainController:
     """Class to provide main controller for Qt GUI."""
 
     def __init__(self):
+        """Class initializer."""
         self.app = QApplication(sys.argv)
+
+        # Setup app translator
+        app_translator = QTranslator()
+
+        translator_pwd = Path(BASE_DIR, VM_PATHS.i18n)
+
+        if translator_pwd.exists():
+            app_translator.load(
+                str(translator_pwd.joinpath('videomorph_{0}'.format(LOCALE))))
+        else:
+            translator_sys_path = Path(SYS_PATHS.i18n,
+                                       'videomorph_{0}'.format(LOCALE))
+            app_translator.load(str(translator_sys_path))
+
+        self.app.installTranslator(app_translator)
+        qt_translator = QTranslator()
+        qt_translator.load("qt_" + LOCALE,
+                           QLibraryInfo.location(
+                               QLibraryInfo.TranslationsPath))
+        self.app.installTranslator(qt_translator)
+
         self.model = None
         self.view = VideoMorphMW()
 
     def run(self):
-        self.view.show()
-        sys.exit(self.app.exec_())
+        """Run the app."""
+
+        # Check for conversion library and run
+        if self.view.conversion_lib.library_path:
+            if len(sys.argv) > 1:  # If it is running from console
+                run_on_console(self.app, self.view)
+            else:  # Or is running on GUI
+                self.view.show()
+                sys.exit(self.app.exec_())
+        else:
+            msg_box = QMessageBox(
+                QMessageBox.Critical,
+                self.view.tr('Error!'),
+                self.view.no_library_msg,
+                QMessageBox.NoButton, self.view)
+            msg_box.addButton("&Ok", QMessageBox.AcceptRole)
+            if msg_box.exec_() == QMessageBox.AcceptRole:
+                qApp.closeAllWindows()
