9b630f1820538b388879b83e799fadd5852b800e
Alex Vergara Gil <alexvergaragil@gmail.com>
2019-07-15 14:34:19 +0200

    adding Homebrew packaging support

---
+++ videomorph.rb
@@ -0,0 +1,23 @@
+class VideoMorph < Formula
+  include Language::Python::Virtualenv
+
+  desc "Video Converter based on ffmpeg"
+  homepage "https://github.com/videomorph-dev/videomorph/"
+  url "https://github.com/videomorph-dev/videomorph/archive/master.zip", :using => :curl
+  sha256 "85c2d58a63053614798a28b6d8a79c046cd3e29518174a364bd2e73b4e9d1f69"
+  head "https://github.com/videomorph-dev/videomorph.git"
+
+  # TODO: If you're submitting an existing package, make sure you include your
+  #       bottle block here.
+
+  depends_on :python3
+  depends_on :ffmpeg
+  depends_on :pyqt
+
+  def install
+    virtualenv_install_with_resources
+    bin.install "setup.py"
+  end
+
+  # TODO: Add your package's tests here
+end
