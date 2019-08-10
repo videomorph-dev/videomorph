b58abb1f868dff3d5507ef0a555f76fd3c38d574
Leodanis Pozo Ramos <lpozor78@gmail.com>
2019-08-10 13:01:34 -0400

    Systray icon issue. Fixed

--- videomorph/forms/videomorph.py
+++ videomorph/forms/videomorph.py
@@ -35,6 +35,7 @@ from PyQt5.QtCore import QPoint
 from PyQt5.QtCore import QProcess
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtWidgets import QVBoxLayout
@@ -785,12 +786,16 @@ class VideoMorphMW(QMainWindow):
                     tagged=self.tag_chb.checkState())
                 # Save settings
                 self._write_app_settings()
+                self.tray_icon.hide()
+                QApplication.instance().quit()
                 event.accept()
             else:
                 event.ignore()
         else:
             # Save settings
             self._write_app_settings()
+            self.tray_icon.hide()
+            QApplication.instance().quit()
             event.accept()
 
     def _fill_media_list(self, files_paths):
